5cc1c06d8224793a60e8851f9a1114e0c7337d8c
hp <hp@DESKTOP-J6P3NN0>
2024-01-11 21:24:14 +0530

    First Commit

---
+++ B49Shop/src/main/java/com/example1/demo/ShopRepository.java
@@ -0,0 +1,17 @@
+package com.example1.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 
+ * @author Bheemaraya
+ *
+ */
+
+
+
+public interface ShopRepository extends JpaRepository<Shop,Integer>{
+
+	
+
+}
